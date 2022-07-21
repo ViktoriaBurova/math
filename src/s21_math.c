9f2b9709967643174cc18cf2b16a2f00e8f91675
crackedg <crackedg@student.21-school.ru>
2022-07-21 20:19:19 +0300

    стартуем

---
+++ src/s21_math.c
@@ -0,0 +1,5 @@
+//
+// Created by Cracked Garnet on 21.07.2022.
+//
+
+// Пушим ветку develop, уважаемые тиммейтики
---
+++ src/s21_math.h
@@ -0,0 +1,8 @@
+//
+// Created by Cracked Garnet on 21.07.2022.
+//
+
+#ifndef C4_S21_MATH_2_S21_MATH_H
+#define C4_S21_MATH_2_S21_MATH_H
+
+#endif //C4_S21_MATH_2_S21_MATH_H
