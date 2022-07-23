6a6a51644f3c974289a80a0cfbf3b01ce826bda9
crackedg <crackedg@student.21-school.ru>
2022-07-23 20:51:55 +0300

    .h

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,8 +1,21 @@
-//
-// Created by Cracked Garnet on 21.07.2022.
-//
-
 #ifndef C4_S21_MATH_2_S21_MATH_H
 #define C4_S21_MATH_2_S21_MATH_H
+
+
+int s21_abs(int x);
+long double s21_acos(double x);
+long double s21_asin(double x);
+long double s21_atan(double x);
+long double s21_ceil(double x);
+long double s21_cos(double x);
+long double s21_exp(double x);
+long double s21_fabs(double x);
+long double s21_floor(double x);
+long double s21_fmod(double x, double y);
+long double s21_log(double x);
+long double s21_pow(double base, double exp);
+long double s21_sin(double x);
+long double s21_sqrt(double x);
+long double s21_tan(double x);
 
 #endif //C4_S21_MATH_2_S21_MATH_H
