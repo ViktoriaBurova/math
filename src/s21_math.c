9173f7d864418f5abd8e0fcfa7758e5a4ddb0252
crackedg <crackedg@student.21-school.ru>
2022-07-24 17:43:36 +0300

    fabs

--- src/s21_math.c
+++ src/s21_math.c
@@ -4,9 +4,11 @@ int s21_abs(int x) {
     return x < 0 ? -x : x;
 }
 
-int main() {
-    printf("MY - %Lf, %Lf\n", s21_cos(15), s21_sin(15));
-    printf("THEY - %f, %f", cos(15), sin(15));
+long double s21_fabs(double x) {
+    long double res;
+    if (x < 0) { res = x * - 1; }
+    else { res = x; }
+    return res;
 }
 
 long double s21_cos(double x) {
@@ -15,7 +17,7 @@ long double s21_cos(double x) {
     p = 0;
     s = 1.0;
     t = 1.0;
-    while(fabs(t / s) > S21_EPS) {
+    while(s21_fabs(t / s) > S21_EPS) {
         p++;
         t = (-t * x * x) / ((2 * p - 1) * (2 * p));
         s += t;
@@ -29,7 +31,7 @@ long double s21_sin(double x) {
     double acc = 1;
     double fact= 1;
     double pow = x;
-    while (fabs(acc) > S21_EPS){
+    while (s21_fabs(acc) > S21_EPS){
         fact *= ((2 * i) * (2 * i + 1));
         pow *= -1 * x * x;
         acc =  pow / fact;
@@ -39,3 +41,7 @@ long double s21_sin(double x) {
     return cur;
 }
 
+int main() {
+    printf("MY - %Lf, %Lf\n", s21_cos(15), s21_sin(15));
+    printf("THEY - %f, %f", cos(15), sin(15));
+}
