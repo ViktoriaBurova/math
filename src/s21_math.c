06f6489da6a4f40cef01a411af5188d17f9c1d65
crackedg <crackedg@student.21-school.ru>
2022-07-31 18:39:27 +0300

    может сдадим

--- src/s21_math.c
+++ src/s21_math.c
@@ -5,24 +5,13 @@ int s21_abs(int x) {
 }
 
 long double s21_fabs(double x) {
-    long double res;
-    if (x < 0) { res = x * - 1; }
-    else { res = x; }
-    return res;
+    if (x < 0) { x *= -1; }
+    return x;
 }
 
-long double s21_cos(double x) {
-    double t , s;
-    int p;
-    p = 0;
-    s = 1.0;
-    t = 1.0;
-    while(s21_fabs(t / s) > S21_EPS) {
-        p++;
-        t = (-t * x * x) / ((2 * p - 1) * (2 * p));
-        s += t;
-    }
-    return s;
+double reader(double x) {
+    while (x > s21_PI || x < -s21_PI) { x += x > s21_PI ? -2 * s21_PI : 2 * s21_PI; }
+    return x;
 }
 
 long double s21_sin(double x) {
@@ -41,11 +30,60 @@ long double s21_sin(double x) {
     return cur;
 }
 
-int s21_abs(int x) {
-    if (x < 0) {
-        x *= -1;
+long double s21_cos(double x) {
+    x = reader(x);
+    return s21_sin((s21_PI / 2.0) - x);
+}
+// tan
+long double s21_tan(double x) {
+    x = reader(x);
+    return s21_sin(x) / s21_cos(x);
+}
+
+long double s21_asin(double x) {
+    long double rez = x, y = x;
+    long double i = 1;
+    while (s21_fabs(rez) > s21_EPS) {
+        if (x < -1 || x > 1) {
+            y = s21_NAN;
+            break;
+        }
+        if (x == 1 || x == -1) {
+            y = s21_PI / 2 * x;
+            break;
+        }
+        rez *= x * x * (2 * i - 1) * (2 * i - 1) / ((2 * i + 1) * 2 * i);
+        i += 1;
+        y += rez;
     }
-    return (unsigned int) x;
+    return y;
+}
+
+long double s21_acos(double x) {
+    if (x <= 1 && x >= -1) {
+        x = s21_PI / 2. - s21_asin(x);
+    } else {
+        x = s21_NAN;
+    }
+    return x;
+}
+
+long double s21_atan(double x) {
+    long double y = 0;
+    y = (x < 1 && x > -1) ? checker(x) : y;
+    y = x == 1 ? s21_PI / 4 : x == -1 ? s21_NPI / 4 : x == 0 ? 0 : y;
+    y = x > 1 ? s21_PI / 2 - checker(1 / x) : x < -1 ? s21_NPI / 2 - checker(1 / x) : y;
+    return y;
+}
+
+long double checker(double x) {
+    long double rez = x, y = x, i = 1;
+    while (s21_fabs(rez) > s21_EPS) {
+        rez = -1 * rez * x * x * (2 * i - 1) / (2 * i + 1);
+        i += 1;
+        y += rez;
+    }
+    return y;
 }
 
 long double s21_ceil(double x) {
@@ -58,17 +96,111 @@ long double s21_ceil(double x) {
 }
 
 long double s21_sqrt(double x) {
-    int i = 2;
-    int num = x;
-    while(i <= num){
-        if(num % i == 0) {
-            printf("%d\n", i);
-            num = num / i;
-            if(num > 1)
-                printf("*\n");
+    long double rez = 4, y = 0;
+    while (s21_fabs(rez - y) > s21_EPS) {
+        if (x < 0) {
+            rez = s21_N_NAN;
+            break;
         }
-        else
-            i++;
+        y = rez;
+        rez = (y + x / y) / 2;
     }
-    return x;
+    return rez;
+}
+
+
+long double s21_floor(double x) {
+    int count;
+    double y = x < 0 ? -x : x;
+    for (count = 0; count < y; count++) {}
+    return x < 0 ? -count : x == 0 ? 0 : count - 1;
+}
+
+long double s21_exp(double x) {
+    long double rez = 1, y = 1;
+    long double i = 1;
+    int flag = 0;
+    if (x < 0) {
+        x *= -1;
+        flag = 1;
+    }
+    while (s21_fabs(rez) > s21_EPS) {
+        rez *= x / i;
+        i++;
+        y += rez;
+        if (y > s21_MAX_double) {
+            y = s21_INF;
+            break;
+        }
+    }
+    y = flag == 1 ? y > s21_MAX_double ? 0 : 1. / y : y;
+    return y = y > s21_MAX_double ? s21_INF : y;
+}
+
+long double s21_fmod(double x, double y) {
+    long double rez = s21_fabs(x);
+    y = s21_fabs(y);
+    int i = 1;
+    while (rez > y) {
+        rez = s21_fabs(x) - y * i;
+        i++;
+    }
+    return x < 0 ? -rez : rez;
+}
+
+long double s21_log(double x) {
+    long double y;
+    return y = (x > 0 && x < 2) ? log_0_2(x) : log_other(x);
+}
+
+long double log_0_2(double x) {
+    x--;
+    long double rez = x, y = x;
+    long double cnt = 2;
+    while (s21_fabs(rez) > s21_EPS) {
+        rez *= -x * (cnt - 1) / cnt;
+        cnt += 1;
+        y += rez;
+    }
+    return y;
+}
+
+long double log_other(double x) {
+    struct special _special;
+    translate(x, &_special);
+    x = _special.mantissa * _special.sign / 10;
+    long double rez;
+    rez = x < 0    ? s21_N_NAN
+                   : x == 0 ? s21_NINF
+                            : log_0_2(x) + (_special.e + 1) * s21_ln10;
+    return rez;
+}
+
+void translate(double x, struct special *_special) {
+    long double i = 1;
+    int es = 0;
+    _special->sign = x < 0 ? -1 : 1;
+    x *= _special->sign;
+    if (x >= 10) {
+        while (x >= 10) {
+            x /= 10.;
+            i *= 10;
+            es++;
+        }
+    } else if (x < 1 && x > 0) {
+        while (x < 1) {
+            x *= 10;
+            i /= 10;
+            es--;
+        }
+    }
+    _special->mantissa = x;
+    _special->pow = i;
+    _special->e = es;
+}
+
+long double s21_pow(double base, double exp) {
+    long double rez;
+    rez = s21_exp(exp * s21_log(base));
+    return rez;
 }
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,11 +1,26 @@
-#ifndef C4_S21_MATH_2_S21_MATH_H
-#define C4_S21_MATH_2_S21_MATH_H
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
 
 #define S21_EPS 1e-17
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
+struct special {
+    int sign;
+    long double mantissa;
+    long double pow;
+    int e;
+};
+
+#define s21_PI 3.14159265358979324
+#define s21_NPI -3.14159265358979324
+#define s21_EPS 1e-17
+#define s21_ln10 2.30258509299404590109
+#define s21_INF 1.0 / 0.0
+#define s21_NINF -1.0 / 0.0
+#define s21_NAN 0.0 / 0.0
+#define s21_N_NAN -0.0 / 0.0
+#define s21_MAX_double 1.7976931348623157e308
 
 int s21_abs(int x);
 long double s21_acos(double x);
@@ -23,4 +38,10 @@ long double s21_sin(double x);
 long double s21_sqrt(double x);
 long double s21_tan(double x);
 
-#endif //C4_S21_MATH_2_S21_MATH_H
+void translate(double x, struct special *_special);
+long double log_other(double x);
+long double log_0_2(double x);
+double reader(double x);
+long double checker(double x);
+
+#endif  // SRC_S21_MATH_H_
